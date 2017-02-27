63cd9c969efa2c7fc37d8d0b6bdc0b15846631c2
Ron Lu <hal0360@rons-imac.telecom>
2017-02-27 20:55:36 +1300

    Initial Commit

---
+++ Clockwork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clockwork
+//
+//  Created by Ron Lu on 27/02/17.
+//  Copyright © 2017 Udenconstruction. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
